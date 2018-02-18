6787be9e08300d5d41dbd23530f08143b3dea922
Trevor Rounce <trevorrounce@Trevors-iMac.hitronhub.home>
2018-02-18 13:27:17 -0700

    setup sep test and dev env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,11 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect( process.env.MONGODB_URI );
 
 
 module.exports = { mongoose };
 
+//  process.env.NODE_ENV === 'production' Heroku
+//  process.env.NODE_ENV === 'development' local run
+//  process.env.NODE_ENV === 'text' mocha test run
